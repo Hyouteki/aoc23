2bb8cc432a69116f2280548c7487f5b28d1c8d5a
Lakshay <lakshay21060@iiitd.ac.in>
2023-12-13 20:28:17 +0530

    completed Day 4 part 1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 > My attempt at solving 2023's [Advent of Code](https://adventofcode.com/2023/)
 
-- [x] Day 1: Trebuchet?!
-- [x] Day 2: Cube Conundrum
-- [x] Day 3: Gear Ratios 
+- [x] Day 01: Trebuchet?!
+- [x] Day 02: Cube Conundrum
+- [x] Day 03: Gear Ratios 
+- [ ] Day 04: Scratchcards
---
+++ day04/part1.rs
@@ -0,0 +1,59 @@
+use std::{io::{BufRead, BufReader}, fs::File};
+use std::collections::HashSet;
+
+fn slurp_file() -> Vec<String> {
+    BufReader::new(File::open("doc.txt")
+        .expect("could not open file")).lines()
+        .map(|line| line.expect("cpuld not parse line"))
+        .collect()
+}
+
+fn parse_num(s: &mut String) -> i32 {
+    let (mut num, mut len) = (0, 0);
+    let mut flag: bool = false;
+    for ch in s.chars() {
+        if ch == ' ' || ch == '|' {
+            if flag {break;}
+            len += 1;
+            continue;
+        }
+        if ch.is_numeric() {
+            num = num*10 + ch as i32 - '0' as i32;
+            len += 1;
+            flag = true;
+        }
+    }
+    *s = s[len..].to_string();
+    num
+}
+
+fn main() {
+    let mut content: Vec<String> = slurp_file();
+    let mut ans: i32 = 0;
+    for i in 0..content.len() {
+        // eat 'Card '
+        content[i] = content[i][5..].to_string();
+        { // eat till ': '
+            let mut x: usize = 1;
+            while content[i].chars().nth(x) != Some(':') {
+                x += 1;
+            }
+            x += 2;
+            content[i] = content[i][x..].to_string();
+        }
+        let mut set: HashSet<i32> = HashSet::new();
+        for _ in 0..10 {
+            set.insert(parse_num(&mut content[i]));
+        }
+        let mut x: i32 = 0;
+        for _ in 0..25 {
+            let num: i32 = parse_num(&mut content[i]);
+            if set.contains(&num) {
+                if x == 0 {x = 1;}
+                else {x <<= 1;}
+            }
+        }
+        ans += x;
+    }
+    println!("ans: {}", ans);
+}
